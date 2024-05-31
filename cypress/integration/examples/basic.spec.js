9942675f6389ced44460803df16b0274f782cbee
Montanher <gabrielmontanherr@gmail.com>
2024-05-31 14:18:13 -0300

    updated

--- cypress/integration/examples/basic.spec.js
+++ cypress/integration/examples/basic.spec.js
@@ -4,7 +4,15 @@ describe('Cypress basics', () => {
     it('should visit a page and assert title', () => {
         cy.visit('https://wcaquino.me/cypress/componentes.html')
 
+        cy.pause() // executa o passo a passo 
+
         cy.title().should('to.equal', 'Campo de Treinamento')
+        cy.title().debug().should('contain', 'Campo')
+
+
+        cy.title()
+            .should('be.equal', 'Campo de Treinamento')
+            .and('contain', 'Campo')
     })
 
     it('Should find and interact with and element', () => {
---
+++ cypress/integration/examples/elements.spec.js
@@ -0,0 +1,8 @@
+/// <reference types="cypress" />
+
+describe('Work with basic elements', () => {
+    it('Text', () => {
+cy.visit('https://wcaquino.me/cypress/componentes.html')        
+cy.get('body').should('contain', 'cuidado')
+    })
+})
